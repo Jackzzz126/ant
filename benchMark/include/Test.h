90df93fbfe40b718bedeb04727acf3052696e596
Jack Zheng <Jackzzz@126.com>
2015-07-04 19:27:44 +0800

    add bench mark.

---
+++ benchMark/include/Test.h
@@ -0,0 +1,10 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+namespace Test
+{
+	void Echo(int packId, char* data, int len);
+}
+
+#endif//_TEST_H_
+
---
+++ benchMark/src/Test.cpp
@@ -0,0 +1,12 @@
+#include "Test.h"
+#include "Log.h"
+
+namespace Test
+{//begin of name space
+
+void Echo(int packId, char* data, int len)
+{
+}
+
+}//end of name space
+
