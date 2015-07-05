209f27b5b96109eb47a21336ad7ce628334112c0
Jack Zheng <Jackzzz@126.com>
2015-07-05 21:06:13 +0800

    change test to benchmark.

---
+++ benchMark/include/BenchMark.h
@@ -0,0 +1,11 @@
+#ifndef _BENCHMARK_H_
+#define _BENCHMARK_H_
+
+	void Init();
+namespace BenchMark
+{
+	void Echo(int packId, char* data, int len);
+}
+
+#endif//_BENCHMARK_H_
+
--- benchMark/src/BenchMark.cpp
+++ benchMark/src/BenchMark.cpp
@@ -1,12 +1,16 @@
-#include "Test.h"
+#include "BenchMark.h"
 #include "Log.h"
 
-namespace Test
+namespace BenchMark
 {//begin of name space
 
 void Echo(int packId, char* data, int len)
 {
 }
 
+
 }//end of name space
+void Init()
+{
+}
 
