443017470041b8e96e8aeb1bbf67f4396df43de2
Jack Zheng <Jackzzz@126.com>
2015-08-13 18:35:49 +0800

    update http.

--- test/http.js
+++ test/http.js
@@ -1,19 +1,38 @@
 var comm = require("./comm");
 
+var reqNum = 10000;
+var sucNum = 0;
+var failNum = 0;
+
+var timeOrigin = (new Date()).getTime();
+
 options = new Object();
 options.path = "/hello";
 options.method = "GET";
 
-comm.httpRequest(options, null, onReturn);
+for(var i = 0; i < reqNum; i++)
+{
+	comm.httpRequest(options, null, onReturn);
+}
 function onReturn(errStr, reply)
 {
-	if(errStr !== null)
+	if(errStr === null)
 	{
-		console.log(errStr);
+		sucNum++;
 	}
 	else
 	{
-		console.log(reply.toString());
+		console.log(errStr);
+		failNum++;
+	}
+	if((sucNum + failNum) % 1000 === 0)
+	{
+		var now = (new Date()).getTime();
+		console.log("suc: %d, fail: %d, in %d ms.", sucNum, failNum, now - timeOrigin);
+	}
+	if((sucNum + failNum) === reqNum)
+	{
+		process.exit(0);
 	}
 }
 
