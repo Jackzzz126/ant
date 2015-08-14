563e1079114e4a1bf27d427c119d78cffa06bf45
Jack Zheng <Jackzzz@126.com>
2015-08-14 11:01:23 +0800

    add login to test.

---
+++ test/login.js
@@ -0,0 +1,57 @@
+var comm = require("./comm");
+
+var ProtoBuf = require("protobufjs");
+var BenchMark = ProtoBuf.loadProtoFile("../proto/benchMark.proto").build("BenchMark");
+
+var userNum = 10000;
+var sucNum = 0;
+var failNum = 0;
+
+var timeOrigin = (new Date()).getTime();
+
+comm.connect(onConnect, onPack);
+function onConnect(socket)
+{
+	var req = new BenchMark.ReqLogin();
+	for(var i = 0; i < userNum; i++)
+	{
+		req.charId = i + 1;
+		req.pwd = "pwd-" + req.charId;
+
+		var dataBuff = req.encode().toBuffer();
+
+		var headBuff = new Buffer(8);
+		headBuff.writeInt32LE(-11 ^ 0x79669966, 0);
+		headBuff.writeInt32LE(dataBuff.length ^ 0x79669966, 4);
+
+		socket.write(headBuff);
+		socket.write(dataBuff);
+	}
+}
+
+function onPack(packId, packLen, buff)
+{
+	var res = BenchMark.ResReg.decode(buff);
+	if(res.status === 0)
+	{
+		sucNum++;
+	}
+	else
+	{
+		console.log(res.status);
+		failNum++;
+	}
+	if((sucNum + failNum) % 1000 === 0)
+	{
+		var now = (new Date()).getTime();
+		console.log("	suc: %d, fail: %d, in %d ms.", sucNum, failNum, now - timeOrigin);
+	}
+	if((sucNum + failNum) === userNum)
+	{
+		var now = (new Date()).getTime();
+		console.log("suc: %d, fail: %d, in %d ms.", sucNum, failNum, now - timeOrigin);
+		process.exit(0);
+	}
+}
+
+
